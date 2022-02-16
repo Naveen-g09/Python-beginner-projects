460fa5711c9e495daf69ad70c3af2f4af40fa0ea
naveen gummella <76151123+Naveen-g09@users.noreply.github.com>
2022-02-16 15:47:43 +0530

    Add files via upload

---
+++ spammer.py
@@ -0,0 +1,10 @@
+
+import pyautogui
+import time as t
+# importing modules and timer
+t.sleep(20)
+
+for i in range(10):
+    pyautogui.press("H")
+    pyautogui.press("I")
+    pyautogui.press("enter")
