ed7fda59a0d0f0b1fe3bcf06e21f16311b8152d4
naveen gummella <76151123+Naveen-g09@users.noreply.github.com>
2022-02-16 15:46:53 +0530

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,11 @@
+# this includes string concatenation
+adj = input("Adjective: ")
+action1 = input("An Action: ")
+verb1 = input("A Verb: ")
+verb2 = input("Another Verb: ")
+
+madlib = f"Computer programming is so {adj}! It makes me feel like to {action1}, daily I am so excited to {verb1} and Code python projects / " \
+         f"that's not it, I have completed a my first {verb2} and madlib project of python"
+
+
+print(madlib)
